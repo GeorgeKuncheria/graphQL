4f518f4ace300107847a6ca6a6a5373a006a97da
GeorgeKuncheria <chempumthara.g@northeastern.edu>
2025-02-26 01:23:41 -0500

    minor changes to index.js

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import {ApolloServer} from "apollo-server";
 
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+    typeDefs,  //typeDefs are the schema that defines the structure of the data
+    resolvers  //resolvers are the functions that run when a query is made
 });
+
+
+server.listen().then(({url})=> {
+    console.log(`Server ready at ${url}`);
+});
+
